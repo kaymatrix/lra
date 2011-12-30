9101b22fafa325d135bb5c0ce8185e0ec32189da
kaymatrix@gmail.com <kaymatrix@gmail.com@f3dbb42c-e4a8-1b5f-f691-cc203e50c901>
2011-12-30 06:26:45 +0000

    fix

---
+++ lib/mAppLog.py
@@ -0,0 +1,60 @@
+#-------------------------------------------------------------------------------
+# Name:        RenderTaskProperties
+# Purpose:
+#
+# Author:      AIAA
+#
+# Created:     15-12-2011
+# Copyright:   (c) AIAA 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+from PyQt4 import QtCore, QtGui
+import oplPyUtilities
+import lra
+import oplQtSupport
+import oplQtConnection
+import oplQtTable
+import oplQtList
+import oplINIRW
+import mIcons
+import mSettings
+import mRenderTask
+import oplQtProcess
+
+class AppLog():
+
+    def __init__(self,parent=''):
+        self._prn=lra.AppStart() if not parent else parent
+        self.timeStamp=False
+        self._prn.p
+
+    def disp(self, data=''):
+        t=self._prn.mUtil.getDateTime()
+        if self.timeStamp:
+            d = '%s %s' + t
+
+    def clean(self):
+        lst = self.parent.rtaskSupport.getAllFlagNames()
+        items = self.parent.qlst.populate(self.parent.lstColumns,lst,False,True)
+        for eachItem in items:
+            self.parent.qsup.setIcon(eachItem, self.parent.mIcon.tag)
+
+    def doPrepareColumns(self):
+        itms = self.parent.qlst.getAllItem(self.parent.lstColumns)
+        for itm in itms:
+            txt = itm.text()
+            chk = itm.checkState()
+            cno = self.parent.qtbl.getHeaderColNo(self.parent.tblMainList, txt)
+            if chk:
+                self.parent.tblMainList.showColumn(cno)
+            else:
+                self.parent.tblMainList.hideColumn(cno)
+        self.parent.qtbl.resizeColumnsEx(self.parent.tblMainList)
+
+    def doSaveList(self):
+        pass
+
+
+
+
